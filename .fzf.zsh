d760cf3f4be91d7a1676d2253de82a10628371f1
yult <yult@qssec.com>
2019-01-14 21:54:03 +0800

    first commit

---
+++ .fzf.zsh
@@ -0,0 +1,14 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/yult/.vim/plugged/fzf/bin* ]]; then
+  export PATH="$PATH:/home/yult/.vim/plugged/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/yult/.vim/plugged/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/yult/.vim/plugged/fzf/shell/key-bindings.zsh"
+
